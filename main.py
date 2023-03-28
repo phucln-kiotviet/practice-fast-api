453af837355b53ed791c5d03a21a6e9f7d48c6cd
papu <papu@papus-MacBook-Pro.local>
2023-03-28 14:45:54 +0700

    test with enum class

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import uvicorn
-from typing import Union
+from enum import Enum
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-  name: str
-  price: float
-  is_offer: Union[bool, None] = None
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-  return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-  return {"item_price": item.price, "item_id": item_id}
-
-
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 if __name__ == '__main__':
   uvicorn.run(app, host='0.0.0.0', port=8000)
