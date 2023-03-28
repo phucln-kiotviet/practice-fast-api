a18a0a5eb4b66177d71de056b7799ca4dbfc0a5c
papu <papu@papus-MacBook-Pro.local>
2023-03-28 17:03:46 +0700

    fastapi request Body + path parameters

--- main.py
+++ main.py
@@ -20,4 +20,12 @@ if __name__ == '__main__':
 
 @app.post("/items")
 async def create_item(item: Item):
-  return item
+  item_dict = item.dict()
+  if item.tax:
+    price_with_tax = item.price + item.tax
+    item_dict.update({"price_with_tax": price_with_tax})
+  return item_dict
+
+@app.put("/items")
+async def create_item(item_id: int, item: Item):
+  return {"item_id": item_id, **item.dict()}
